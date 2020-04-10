4044c7d0cc40cdb9dade18f595b4017372ae0e2b
Afnan Sultan <sultan.afnan@gmail.com>
2020-04-10 19:51:46 -0400

    FileController and default Users folder
    FileController creates a new folder called <UserId> upon registration.
    The Users/<UserId> directory is where we will store the images

---
+++ ConnectumAPI/BackendServices/FileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using ConnectumAPI.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConnectumAPI.BackendServices
+{
+    public class FileController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private void SetUserFolder(User userId)
+        {
+            string path = @".\Users\userId";
+
+            try
+            {
+                if ( Directory.Exists(path))
+                {
+                    return;
+                }
+
+                // Try to create the directory
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            finally {}
+        }
+    }
+}
